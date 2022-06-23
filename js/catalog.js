033685831aed9de6fc692635dbfa35e6d4fda7c4
Nicholas Leonard <nicholas_leonard@live.com>
2022-06-23 19:29:31 -0400

    created function to populate drop down list

--- js/catalog.js
+++ js/catalog.js
@@ -12,7 +12,9 @@ function populateForm() {
   //TODO: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
   for (let i in Product.allProducts) {
-
+    let op = document.createElement('option');
+    op.textContent = Product.allProducts[i].name;
+    selectElement.appendChild(op);
   }
 
 }
@@ -23,6 +25,7 @@ function populateForm() {
 function handleSubmit(event) {
 
   // TODO: Prevent the page from reloading
+  event.preventDefault();
 
   // Do all the things ...
   addSelectedItemToCart();
@@ -37,6 +40,7 @@ function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
+  
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
