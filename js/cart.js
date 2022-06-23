4bcd14b93777daa80366e9a2b56b13a7045c6b21
Timothy Parker <tlparker71@gmail.com>
2022-06-23 16:46:53 -0700

    added cart.js

--- js/cart.js
+++ js/cart.js
@@ -19,10 +19,35 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  document.getElementById('tbody').removeChild('tr');
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
+  for (let i = 0; i < this.cartItems.length; i++) {
+    const tr = document.createElement('tr');
+    tr.cartContent = this.cartItems[i];
+    
+    tbody.appendChild(tr);
+  }
+    
+  const dataRow = document.createElement('tr');
+  tbody.appendChild(dataRow);
+
+    const deleteLink = document.createElement('td');
+    dataRow.appendChild(deleteLink);
+    deleteLink.addEventListener('click', removeItemFromCart);
+
+    const quantityCell = document.createElement('td');
+    dataRow.appendChild(quantityCell);
+    quantityCell.textContent = this.quantity;
+
+    const item = document.createElement('td');
+    dataRow.appendChild(item);
+    item.textContent = this.product;
+
+    tbody.appendChild(dataRow);
 
   // TODO: Find the table body
 
@@ -34,7 +59,11 @@ function showCart() {
 }
 
 function removeItemFromCart(event) {
-
+  Cart.prototype.removeItem();
+  Cart.prototype.saveToLocalStorage();
+  document.getElementById('tr').removedChild('td');
+  const deleteLink = document.createElement('td');
+    dataRow.appendChild(deleteLink);
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
   // TODO: Save the cart back to local storage
   // TODO: Re-draw the cart table
